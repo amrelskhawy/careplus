446c109b9c802dfe4cd615d791590ab3fea0ca26
Amr Elskhawy <amrelskhawy@gmail.com>
2024-08-13 23:34:32 +0300

    Create Patient Form to Register The Patient to the system

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -1,7 +1,63 @@
-import React from 'react'
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Button } from "../ui/button"
+import { Form } from "@/components/ui/form"
+import { CutomFormField } from "../CutomFormField"
+
+export enum FormFieldType {
+  INPUT = "input" ,
+  CHECKBOX = "checkbox",
+  TEXTAREA = "textarea",
+  DATE_PIKER = "datePicker",
+  PHONE_INPUT = "phoneInput",
+  SELECT = "select",
+  SKELETON = "skeleton"
+}
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message : "username should be 2 letters or more"
+  }).max(50)
+})
 
 export const PatientForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: ""
+    }
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+    
+  }
+
   return (
-    <div>PatientForm</div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <section className="mb-12 space-y-4">
+          <h1 className="header">Hi there 👋</h1>
+          <p className="text-dark-700">Schedule your first appointment.</p>
+        </section>
+
+        {/* Username Input */}
+        <CutomFormField 
+          control={form.control}
+          name={'username'}
+          label={"Username"}
+          fieldType={FormFieldType.INPUT}
+          placeholder={""}
+          desc={""} 
+          iconSrc={"user.svg"} 
+          iconAlt={""}          
+          />
+
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
   )
 }
